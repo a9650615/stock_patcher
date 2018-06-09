82f3046e46a42e3ce6c0d893cbc687e04c22d520
菜鳥祐 <a9650615@gmail.com>
2018-06-09 12:00:57 +0800

    range patch

--- src/index.js
+++ src/index.js
@@ -3,15 +3,17 @@ import StockHistory from './patcher/stockHistory'
 
 let data = {
   date: null,
-  range: false
+  startDate: null,
+  toDate: null
 }
 
 const main = async () => {
+  console.log(data.startDate, data.toDate)
   if (data.date) {
     (await StockHistory.getData(data.date)).toFiles()
   } 
-  else if (data.range) {
-    (await StockHistory.getDataRange('2015/01/02'))
+  else if (data.startDate && data.toDate) {
+    (await StockHistory.getDataRange(data.startDate, data.toDate)).toFiles()
   }
   else {
     console.log('未執行任何動作')
@@ -26,9 +28,20 @@ const setDate = (date) => {
   }
 }
 
-const setRange = (date) => {
-  console.log('range')
-  data.range = true
+const setStart = (date) => {
+  if (Date.parse(date)) {
+    data.startDate = date
+  } else {
+    console.warn('-f: 請輸入正確的時間格式')
+  }
+}
+
+const setTo = (date) => {
+  if (Date.parse(date)) {
+    data.toDate = date
+  } else {
+    console.warn('-t: 請輸入正確的時間格式')
+  }
 }
 
 Program
@@ -39,7 +52,15 @@ Program
   .description('爬取資料')
   .option('-s, --single', 'Get Single date data')
   .option('-d, --date [date]', 'Select a date', setDate)
-  .option('-r, --range', 'Select a date range', setRange)
+  .action((env) => {
+    main()
+  })
+
+Program
+  .command('batch-patch')
+  .description('爬取範圍')
+  .option('-f, --from [date]', 'from date', setStart)
+  .option('-t, --to [date]', 'to date', setTo)
   .action((env) => {
     main()
   })
--- src/patcher/stockHistory.js
+++ src/patcher/stockHistory.js
@@ -8,7 +8,9 @@ class StickqHistory extends Base {
     const day = ('0' + date.getDate()).slice(-2)
     await this.getWebSiteByUrl(`http://www.stockq.org/stock/history/${year}/${month}/${year}${month}${day}_tc.php`)
     console.log(`Getting: http://www.stockq.org/stock/history/${year}/${month}/${year}${month}${day}_tc.php`)
-    this._parseData()
+    this._parseData({
+      date: `${year}/${month}/${day}`
+    })
 
     return this
   }
@@ -29,37 +31,47 @@ class StickqHistory extends Base {
       })
     })
     this.data = data
-    console.log(data)
   }
 
-  async getDataRange(data) {
-    const date = new Date(Date.parse(data))
-    const year = date.getFullYear()
-    const month = ('0' + (date.getMonth() + 1)).slice(-2)
-    const day = ('0' + date.getDate()).slice(-2)
-    await this.getWebSiteByUrl(`http://www.stockq.org/stock/history/${year}/${month}/${year}${month}${day}_tc.php`)
-    console.log(`Getting: http://www.stockq.org/stock/history/${year}/${month}/${year}${month}${day}_tc.php`)
-    this._parseDataRange()
+  async getDataRange(from, to) {
+    const fromTime = new Date(from).getTime()/1000
+    const toTime = new Date(to).getTime()/1000
+    console.log(toTime-fromTime)
+    let data = {}
+    if (toTime > fromTime) {
+      for(let dateTime = fromTime; dateTime <= toTime; dateTime += 86400) {
+        const date = new Date(dateTime*1000)
+        const year = date.getFullYear()
+        const month = ('0' + (date.getMonth() + 1)).slice(-2)
+        const day = ('0' + date.getDate()).slice(-2)
+        await this.getWebSiteByUrl(`http://www.stockq.org/stock/history/${year}/${month}/${year}${month}${day}_tc.php`)
+        console.log(`Getting: http://www.stockq.org/stock/history/${year}/${month}/${year}${month}${day}_tc.php`)
+        this._parseDataRange(data, `${year}/${month}/${day}`)
+      }
+    }
     return this
   }
 
-  _parseDataRange() {
-    let data = {}
+  _parseDataRange(data, date) {
     this.html('.marketdatatable,.bonddatatable').each((index, value) => {
       const type = this.html(value).find('tr:nth-child(1)>td b').text().trim()
       const rowSize = this.html(value).find('tr:nth-child(2)>td').length
       // console.log(rowSize)
       this.html(value).find('tr.row1, tr.row2').each((index , element) => {
         const detailType = this.html(element).find('td:nth-child(1)').text()
-        let tmpData = {}
-        data[`${type}-${detailType}`.replace('/','')] = []
-        console.log(`${type}-${detailType}`)
-        // for(let i=1 ; i<rowSize ; i++) {
-        //   tmpData[this.html(value).find('tr:nth-child(2)>td').eq(i).text()] = this.html(element).find('td').eq(i).text()
-        // }
-        // data[type].push(tmpData)
+        const lastType = `${type}-${detailType}`.replace('/','')
+        if (!data[lastType])
+          data[lastType] = []
+        let tmpData = {'時間': date}
+        for(let i=1 ; i<rowSize ; i++) {
+          // data[lastType].push(this.html(element).find('td').eq(i).text())
+          tmpData[this.html(value).find('tr:nth-child(2)>td').eq(i).text()] = this.html(element).find('td').eq(i).text()
+        }
+        // console.log(data[lastType].length)
+        data[lastType].push(tmpData)
       })
     })
+    // console.log(data)
     this.data = data
   }
 }
